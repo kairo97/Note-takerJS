72a07148c03660e47a3cd2ff5b3a0f4a140a62d5
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-28 19:45:38 -0800

    got filepathing working correctly to load  main page

--- assets/js/server.js
+++ assets/js/server.js
@@ -5,9 +5,16 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlenconded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+const noteRoutes = require("../../controllers/taskController")
+app.use("../db", noteRoutes)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
 
 app.listen(PORT, function(){
     console.log(`listening on port` + PORT);
